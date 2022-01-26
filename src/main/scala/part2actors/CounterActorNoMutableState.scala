fc82015bfbad176cb13f587c80644556098f046a
Gabriel Răileanu <raileanu.gabriel97@gmail.com>
2022-01-26 23:40:31 +0200

    14. Dummy solution for votingSystem * Changing actors behavior - exercises

--- src/main/scala/part2actors/CounterActorNoMutableState.scala
+++ src/main/scala/part2actors/CounterActorNoMutableState.scala
@@ -1,15 +1,38 @@
 package part2actors
 
-import akka.actor.{Actor, ActorSystem}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import part2actors.ActorCapabilities.Counter.Increment
+import part2actors.CounterActorNoMutableState.Counter.{Decrement, Print}
 
 object CounterActorNoMutableState extends App {
-  val system = ActorSystem("conterActorNoMutableState")
+  val system = ActorSystem("counterActorNoMutableState")
+
   /**
    * Exercises
    * 1 - recreate the Counter Actor & no mutable state
    */
 
-  class Counter extends Actor {
-    override def receive: Receive = ???
+  object Counter {
+    case object Increment
+
+    case object Decrement
+
+    case object Print
   }
+
+  class Counter extends Actor {
+    override def receive: Receive = countReceive(0)
+
+    // "Receive" type is actually Actor.Receive, which is actually PartialFunction[Any, Unit]
+    def countReceive(currentCount: Int): PartialFunction[Any, Unit] = {
+      case Increment => context.become(countReceive(currentCount + 1))
+      case Decrement => context.become(countReceive(currentCount - 1))
+      case Print => println(s"[counter] my counter is $currentCount")
+    }
+  }
+
+  val counter: ActorRef = system.actorOf(Props[Counter], "myCounter")
+  (1 to 5).foreach(_ => counter ! Increment)
+  (1 to 3).foreach(_ => counter ! Decrement)
+  counter ! Print
 }
--- src/main/scala/part2actors/VotingSystem.scala
+++ src/main/scala/part2actors/VotingSystem.scala
@@ -1,20 +1,33 @@
 package part2actors
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
-import part2actors.ChangingActorBehavior.system
+
+import scala.collection.mutable
 
 object VotingSystem extends App {
   val system = ActorSystem("votingSystemDemo")
-  /**
-   * Exercise  2 - simplified voting system
-   */
-
-  case object VoteStatusRequest
+  val alice = system.actorOf(Props[Citizen])
+  val bob = system.actorOf(Props[Citizen])
+  val charlie = system.actorOf(Props[Citizen])
+  val daniel = system.actorOf(Props[Citizen])
+  val voteAggregator = system.actorOf(Props[VoteAggregator])
 
   case class VoteStatusReply(candidate: Option[String])
 
   class Citizen extends Actor {
-    override def receive: Receive = ??? // TODO
+
+    var myOption: String = ""
+
+    override def receive: Receive = {
+      case Vote(value) => {
+        println(s"I vote for $value.")
+        myOption = value
+      }
+      // store with who I voted with
+      case VoteStatusRequest =>
+        println(s"Sent my vote for ${myOption}")
+        sender() ! VoteStatusReply(Some(myOption))
+    }
   }
 
   case class Vote(candidate: String)
@@ -22,21 +35,36 @@ object VotingSystem extends App {
   case class AggregateVotes(citizens: Set[ActorRef])
 
   class VoteAggregator extends Actor {
-    override def receive: Receive = ???
+
+    val candidateAndNoVotes: mutable.Map[String, Int] = scala.collection.mutable.Map()
+
+    override def receive: Receive = {
+      case AggregateVotes(citizens) => {
+        println("Let's aggregate votes")
+        citizens.foreach(_ ! VoteStatusRequest)
+      }
+      case VoteStatusReply(reply) =>
+        println("Received voteStatusReply")
+        reply.map(candidateName => {
+          val currentValue = candidateAndNoVotes.getOrElse(candidateName, 0)
+          candidateAndNoVotes.put(candidateName, currentValue + 1)
+        }
+        )
+        println(candidateAndNoVotes.map(pair => pair._1 + "=" + pair._2).mkString(","))
+    }
   }
-
-
-  val alice = system.actorOf(Props[Citizen])
-  val bob = system.actorOf(Props[Citizen])
-  val charlie = system.actorOf(Props[Citizen])
-  val daniel = system.actorOf(Props[Citizen])
 
   alice ! Vote("Martin")
   bob ! Vote("Jonas")
   charlie ! Vote("Roland")
   daniel ! Vote("Roland")
 
-  val voteAggregator = system.actorOf(Props[VoteAggregator])
+  /**
+   * Exercise  2 - simplified voting system
+   */
+
+  case object VoteStatusRequest
+
   voteAggregator ! AggregateVotes(Set(alice, bob, charlie, daniel))
 
   /*
